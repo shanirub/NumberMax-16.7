fb5dd0c9bae909948df8e5c9a53b76cb123a7839
srub <shani.rubovitch@gmail.com>
2021-10-03 09:28:59 +0300

    added exception to handle x=0 and y=0

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ if __name__ == '__main__':
     x = int(input("first number: "))
     y = int(input("second number: "))
 
-    max_number = find_max(x, y)
+    try:
+        max_number = find_max(x, y)
+    except ZeroDivisionError:
+        max_number = 0
 
     print("max number is: " + str(max_number))
 
